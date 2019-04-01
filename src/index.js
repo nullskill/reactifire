d8bb85f6b92fd8fa712f80cb4c973ec6127f91d0
nullskill <nullskill@github.com>
2019-04-01 22:47:26 +0300

    Add hot module replacement

--- src/index.js
+++ src/index.js
@@ -7,12 +7,23 @@ import * as serviceWorker from "./serviceWorker";
 
 import store, { history } from "./store";
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App history={history} />
-  </Provider>,
-  document.getElementById("root")
-);
+const render = () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <App history={history} />
+    </Provider>,
+    document.getElementById("root")
+  );
+};
+
+render();
+
+// Hot reloading
+if (module.hot) {
+  module.hot.accept("./App", () => {
+    setTimeout(render);
+  });
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
